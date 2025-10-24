7543da996e6eb5100c1728f9a92dc8cd2ce3f572
Jacob <jacobrmason@gmail.com>
2025-10-24 14:15:10 -0600

    reorganize repo

--- test-onnx.js
+++
@@ -1,7 +0,0 @@
-const ort = require('onnxruntime-node');
-
-if (ort.InferenceSession) {
-  console.log('ONNX Runtime loaded successfully.');
-} else {
-  console.log('ONNX Runtime failed to load.');
-}
